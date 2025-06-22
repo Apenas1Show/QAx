274a510501c85e20e9d0f94348555fa2ecf7fc45
Apenas1Show <rafaelbertolai2@gmail.com>
2025-06-21 22:40:10 -0300

    delete de todos os arquivos antigos do projeto

--- projects/playwright-mark/tests/home.spec.ts
+++
@@ -1,8 +0,0 @@
-import {test, expect} from '@playwright/test'
-
-test('webapp deve estar online', async ({page})=> {
-    await page.goto('http://192.168.161.1:8080') // abra a página 'link da página'
-    await expect(page).toHaveTitle('Gerencie suas tarefas com Mark L') // que o titulo do HTML seja Gerencie suas tarefas com Mark L
-    await (page).waitForTimeout(3000) // aguarde carregar 3 segundos
-})
-
